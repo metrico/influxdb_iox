ce1186f82f8ba10e2f90944b41b86b1d40d779a0
Fraser Savage <fsavage@influxdata.com>
2023-09-22 16:59:48 +0100

    test(ingester): Add unit testing to cover `CatalogMapResolver` behaviour

--- ingester/src/persist/column_map_resolver/catalog.rs
+++ ingester/src/persist/column_map_resolver/catalog.rs
@@ -45,12 +45,86 @@ impl ColumnMapResolver for CatalogMapResolver {
                 .find(|sort_key_column| !column_map.contains_column_name(sort_key_column))
             {
                 panic!(
-                    "sort key column {} not present in column map {:?} for table id {}",
-                    sort_key_column, column_map, table_id
+                    "sort key column {} not present in column map for table id {}",
+                    sort_key_column, table_id
                 )
             }
         }
 
         column_map
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use data_types::Column;
+    use iox_catalog::mem::MemCatalog;
+
+    use crate::test_util::{
+        populate_catalog_with_table_columns, ARBITRARY_NAMESPACE_NAME, ARBITRARY_TABLE_NAME,
+    };
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_load_verified_column_map() {
+        let metrics = Arc::new(metric::Registry::default());
+        let catalog: Arc<dyn Catalog> = Arc::new(MemCatalog::new(metrics));
+
+        let columns = ["bananas", "uno", "dos", "tres"];
+
+        // Populate the catalog with the namespace, table & columns
+        let (_ns_id, table_id, col_ids) = populate_catalog_with_table_columns(
+            &*catalog,
+            &ARBITRARY_NAMESPACE_NAME,
+            &ARBITRARY_TABLE_NAME,
+            &columns,
+        )
+        .await;
+
+        let resolver = CatalogMapResolver::new(catalog);
+
+        let got_map = resolver
+            .load_verified_column_map(table_id, Some(&SortKey::from_columns(columns)))
+            .await;
+
+        assert_eq!(
+            got_map,
+            ColumnsByName::new(columns.into_iter().zip(col_ids).map(|(name, id)| {
+                Column {
+                    id,
+                    table_id,
+                    name: name.to_string(),
+                    column_type: data_types::ColumnType::Tag,
+                }
+            }))
+        );
+    }
+
+    #[tokio::test]
+    #[should_panic(
+        expected = r#"sort key column bananas not present in column map for table id 1"#
+    )]
+    async fn test_load_verified_column_map_panics() {
+        let metrics = Arc::new(metric::Registry::default());
+        let catalog: Arc<dyn Catalog> = Arc::new(MemCatalog::new(metrics));
+
+        let columns = ["uno", "dos", "tres"];
+
+        // Populate the catalog with the namespace, table & columns
+        let (_ns_id, table_id, _col_ids) = populate_catalog_with_table_columns(
+            &*catalog,
+            &ARBITRARY_NAMESPACE_NAME,
+            &ARBITRARY_TABLE_NAME,
+            &columns,
+        )
+        .await;
+
+        let resolver = CatalogMapResolver::new(catalog);
+
+        // Load with a on
+        resolver
+            .load_verified_column_map(table_id, Some(&SortKey::from_columns(["bananas"])))
+            .await;
     }
 }
